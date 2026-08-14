71b3157ec72fb7322a7c846296cd8df583c0aef9
Mano Ranjith <kmranjith12389@gmail.com>
2026-08-14 07:48:47 +0000

    update

--- CharacterCount.java
+++ CharacterCount.java
@@ -1,21 +1,51 @@
 import java.util.HashMap;
-class CharacterCount
-{
+public class CharacterCount {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	public static void main(String[] args) {
+		String inputString = "HELLO THERE";
+		HashMap<Character,Integer>charCountMap=countCharacters(inputString);
+		printCharacterCount(charCountMap);
+	}
+	public static HashMap<Character, Integer>countCharacters(String inputString)
+	{
+		HashMap<Character, Integer> charCountMap = new HashMap<>();
+		for(int i=0; i<inputString.length(); i++)
+		{
+			char c = inputString.charAt(i);
+			if(Character.isLetter(c))
+			{
+				c = Character.toUpperCase(c);
+				if(charCountMap.containsKey(c))
+				{
+					charCountMap.put(c, charCountMap.get(c)+1);
+				}
+				else
+				{
+					charCountMap.put(c, 1);
+				}
+			}
+		}
+			return charCountMap;
+			}
+		public static void printCharacterCount(HashMap<Character, Integer>charCountMap)
+		{
+			for(char c: charCountMap.keySet())
+			{
+				System.out.println(c+ ":"+ charCountMap.get(c));
+			}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
